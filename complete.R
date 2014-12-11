2cd40bf88f2b16ddc84596ca7a55a53569329698
etristan <beltar32@hotmai.com>
2014-12-11 11:59:20 +0100

    Create complete.R

---
+++ complete.R
@@ -0,0 +1,18 @@
+complete <- function(directory, id = 1:332) {
+  directory = dirname(directory)
+  class(directory)
+  csvs <- character(length=0)
+  for(i in id) {
+    csvs <- c(csvs, dir(path=directory, pattern = sprintf("%03d.csv", i, full.names = TRUE)))
+  }
+  nobs <- c()
+  for(i in 1:length(csvs)){
+    nobs[i] <- sum(complete.cases(read.csv(csvs[i])))
+    #print(nobs)
+    #ldf <- rbind(ldf,data.frame(read.csv(csvs[i])))
+    #print(csvs[i])}
+  }
+  data <- data.frame(id, nobs)
+  
+  data
+}
