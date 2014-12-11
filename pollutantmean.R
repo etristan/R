f29d8d4749f4fd32afab944016a0fc4b19e760cc
etristan <beltar32@hotmai.com>
2014-12-11 12:08:09 +0100

    Update pollutantmean.R

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,19 +1,17 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
+  #in order to use this function, the working directory must be where de "*.csv"'s are.
   directory = dirname(directory)
   csvs <- character(length=0)
   for(i in id) {
-
     csvs <- c(csvs, dir(path=directory, pattern = sprintf("%03d.csv", i, full.names = TRUE)))
-
-
   }
-  #csvs <- dir(path=directory, pattern = sprintf("%03d.csv", id), full.names = TRUE)
-  #print(csvs)
+  ###csvs <- dir(path=directory, pattern = sprintf("%03d.csv", id), full.names = TRUE)
+  ###print(csvs)
   ldf <- data.frame(x = numeric(), y = character(), stringsAsFactors = FALSE)
   for(i in 1:length(csvs)){
     ldf <- rbind(ldf, data.frame(read.csv(csvs[i])))
   print(csvs[i])}
-  #print(ldf)
+  ###print(ldf)
   if(pollutant == "nitrate") {
   meana <- mean(ldf$nitrate, na.rm = TRUE)
   } else if(pollutant == "sulfate") {
